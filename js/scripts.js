2341b8760cce322fcea37c3c772bb0b69e507af6
Adam <wonton916@gmail.com>
2015-09-22 20:13:57 -0700

    add scripts.js and spec.js to pizza_store

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+function Pizza(quantity, topping, size){
+  var toppings = [];
+  toppings.push(topping);
+  this.quantity = quantity;
+  this.toppings = toppings;
+  this.size = size;
+};
+
+Pizza.prototype.addTopping = function(new_topping){
+  this.toppings.push(new_topping);
+};
+
+Pizza.prototype.totalPrice = function(){
+  var basePrice = 15;
+  basePrice = (this.toppings.length * 2) + basePrice;
+  switch(this.size){
+    case "small":
+        basePrice = 0 + basePrice;
+        break;
+    case "large":
+        basePrice = 5 + basePrice;
+        break;
+  }
+  price = this.quantity*(price);
+  return price;
+
+}
---
+++ spec/specs.js
@@ -0,0 +1,21 @@
+describe('Pizza', function(){
+  it("creates a pizza", function(){
+    var testPizza = new Pizza();
+    expect(testPizza).to.eql(new Pizza());
+  });
+  it("it returns the quantity of pizza ordered", function(){
+    var testPizza = new Pizza(3, "pepperoni", "large");
+    expect(testPizza.quantity).to.equal(3);
+  });
+
+  it("it checks for the toppings that was inputed", function(){
+    var testPizza = new Pizza(3, "pepperoni", "large");
+    expect(testPizza.toppings).to.eql(["pepperoni"]);
+  });
+
+  it("it checks for the pizza size that was inputed", function(){
+    var testPizza = new Pizza(3, "pepperoni", "large");
+    expect(testPizza.size).to.equal("large");
+  });
+
+});
