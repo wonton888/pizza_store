337207809cc688e441383419842920e96ffc62dc
Diane Douglas <diane.douglas1@gmail.com>
2015-09-24 17:06:07 -0700

    Add notes from code review with diane.

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,18 @@ Pizza.prototype.totalPrice = function(){
   basePrice = (this.toppings.length * 2) + basePrice;
   switch(this.size){
     case "small":
-        basePrice = 0 + basePrice;
+        // basePrice = 0 + basePrice;
+        // No need to modify basePrice for a small
         break;
     case "large":
         basePrice = 5 + basePrice;
         break;
   }
-  basePrice = this.quantity*(basePrice);
-  return basePrice;
+  // basePrice = this.quantity*(basePrice);
+  // nothing wrong with previous line, but for clarity it could be nice to give it a new name besides basePrice
+  // alternately, could just use the name price throughout, because then it doesn't look like you are accidentally returning the same starting price.
+  var finalPrice = this.quantity*(basePrice);
+  return finalPrice;
 
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -2,7 +2,7 @@ describe('Pizza', function(){
   it("creates a pizza", function(){
     var testPizza = new Pizza();
     expect(testPizza).to.eql(new Pizza());
-  });
+});//not needed, but doesn't hurt.
   it("it returns the quantity of pizza ordered", function(){
     var testPizza = new Pizza(3, "pepperoni", "large");
     expect(testPizza.quantity).to.equal(3);
@@ -18,9 +18,14 @@ describe('Pizza', function(){
     expect(testPizza.size).to.equal("large");
   });
 
-  it("finds the total cost of the pizza ordered", function(){
+  it("finds the total cost of 3 large pizzas ordered with one topping", function(){
     var testPizza = new Pizza(3, "pepperoni", "large");
     expect(testPizza.totalPrice()).to.equal(66);
+  });
+
+  it("finds the total cost of 3 small pizzas ordered with one topping", function(){
+    var testPizza = new Pizza(3, "pepperoni", "small");
+    expect(testPizza.totalPrice()).to.equal(51);
   });
 
 });
